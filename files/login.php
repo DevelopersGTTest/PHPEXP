9286cc0a1b5f20dd3d75b91f44a8f4194906820a
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-11-05 15:38:16 -0600

    Fix POST DATA

--- files/login.php
+++ files/login.php
@@ -2,9 +2,34 @@
 
 require_once 'conexion.php';
 
+$ins = false;
+
 $sql = "SELECT * FROM bus";
 $array = $pdo->query($sql);
 $result = $array->fetchAll();
+
+if(!empty($_POST)){
+    $codigo_barra = $_POST['codigo_barra'];
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $id_bus_manana = $_POST['id_bus_manana'];
+    $id_bus_tarde = $_POST['id_bus_tarde'];
+
+    $insrt = "INSERT INTO config_prelogin(codigo_barra, nombres, apellidos,id_bus_manana, id_bus_tarde)
+    VALUES(:codigo_barra, :nombres, :apellidos, :id_bus_manana, :id_bus_tarde)";
+
+    $consulta = $pdo->prepare($insrt);
+   
+
+    $ins = $consulta->execute([
+        'codigo_barra' =>  $codigo_barra,
+        'nombres'=> $nombres,
+        'apellidos' => $apellidos,
+        'id_bus_manana' => $id_bus_manana,
+        'id_bus_tarde' => $id_bus_tarde, 
+    ]);
+
+}
 
 
 ?>
@@ -19,16 +44,31 @@ $result = $array->fetchAll();
 </head>
 <body>
   
-   
-<!-- Dinamico -->
-<select>
-<?php
+  <form action="login.php" method="POST" >
+    <label for="">codigo_barra</label>
+    <input type="number"  name="codigo_barra" id="codigo_barra" placeholder="Ingrese su codigo de barras">
+    <label for="nombres">nombres</label>
+    <input type="text"  name="nombres" id="nombres" placeholder="Ingrese sus nombres">
+    <label for="">apellidos</label>
+    <input type="text" name="apellidos" id="apellidos" placeholder="ingrese sus apellidos">
+    <label for="">bus por la mañana</label>
+    <select name="id_bus_manana" id="id_bus_manana" >
+    <?php
     foreach($result as $val){
         echo "<option value=".$val['id_bus'].">". $val['nombre_bus'] . "</option>";
     }
-                      
-   ?>  
-</select>
+    ?>
+    </select>
+    <label for="">bus por la tarde</label>
+    <select name="id_bus_tarde" id="id_bus_tarde" >
+    <?php
+    foreach($result as $val){
+        echo "<option  value=".$val['id_bus'].">". $val['nombre_bus'] . "</option>";
+    }
+    ?>                                  
+    </select>
+    <input type="submit" value="Save" >
+  </form>
 
-</body>
+
 </html>
