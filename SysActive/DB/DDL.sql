b33195ddfa4d9a66150714bdde07e1bf40eb7b6e
unknown <pablojacobo1998@gmail.com>
2018-11-25 22:03:35 -0600

    DDL finished

---
+++ SysActive/DB/DDL.sql
@@ -0,0 +1,26 @@
+CREATE TABLE usuarios(
+	id_usuario INT NOT NULL AUTO_INCREMENT,
+    nombres VARCHAR(255) NOT NULL,
+    apellidos VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    pass VARCHAR(255),
+	PRIMARY KEY(id_usuario)
+);
+
+
+CREATE TABLE categoria(
+	id_categoria INT NOT NULL AUTO_INCREMENT,
+    nombre_categoria VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id_categoria)
+);
+
+
+CREATE TABLE post(
+	id_post INT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    id_categoria INT NOT NULL,
+    id_usuario INT NOT NULL,  
+    PRIMARY KEY(id_post),
+    FOREIGN KEY(id_categoria) REFERENCES categoria(id_categoria),
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
