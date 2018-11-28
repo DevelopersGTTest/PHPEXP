14bbe55df802bbd3adcebfb26948e430c1304ee2
unknown <pablojacobo1998@gmail.com>
2018-11-27 23:51:04 -0600

    dd POST

--- SysActive/includes/login.php
+++ SysActive/includes/login.php
@@ -15,12 +15,12 @@
 
 <div class="container">
 
-<form class="form-signin">
+<form class="form-signin" method="POST" action="../controllers/controller_login.php" >
     <h2 class="form-signin-heading">Coloca tus credenciales</h2>
     <label for="inputEmail" class="sr-only">Email</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+    <input type="email" id="inputEmail" class="form-control" name="email"  placeholder="Email address" required="" autofocus="">
     <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+    <input type="password" id="inputPassword" class="form-control" name="pass" placeholder="Password" required="">
     <button class="btn btn-lg btn-primary btn-block" type="submit">Acceder</button>
 </form>
 
