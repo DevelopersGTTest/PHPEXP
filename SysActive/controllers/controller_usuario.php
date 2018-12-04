9cbc30c60c746e3137aca70a89e865eca9b7ddec
unknown <pablojacobo1998@gmail.com>
2018-12-03 22:10:45 -0600

    Adding Funtionality Cntrl

--- SysActive/controllers/controller_usuario.php
+++ SysActive/controllers/controller_usuario.php
@@ -1,6 +1,27 @@
 <?php
 
-if(isset($_POST)){
-    var_dump($_POST);
-}
+    if(isset($_POST)){
+        
+        session_start();
+        require_once '../includes/conexion.php';
+
+        $nombres =    isset($_POST['nombres']) ?  $_POST['nombres'] : false ;
+        $apellidos =  isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+        $email =      isset($_POST['email']) ? trim($_POST['email']) : false;
+
+        if(isset($_SESSION['id_usuario'])){
+            $sql = "UPDATE usuarios SET
+            nombres='$nombres', apellidos='$apellidos', email='$email'
+                WHERE id_usuario = ". $_SESSION['id_usuario'];
+            
+            $execute = mysqli_query($db, $sql);
+
+            if($execute){
+                $_SESSION['datos_actualizados'] = 'Tus datos se han actualizado correctamente' ;
+            }else{
+                echo 'hubo un error';
+            }
+        }
+    }
+    header("Location: ../includes/perfil.php")
 ?>
