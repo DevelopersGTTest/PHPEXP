c7050e3df82d668b5ed50597fd10502e068df16a
unknown <pablojacobo1998@gmail.com>
2018-12-16 22:26:08 -0600

    Conexion Refactor

--- SysActive/includes/conexion.php
+++ SysActive/includes/conexion.php
@@ -5,14 +5,18 @@
     $pass = '';
     $dbName = 'sisdev';
 
-    $db = mysqli_connect($server, $user, $pass, $dbName);
+    try{
 
-    mysqli_query($db, "SET NAMES 'utf8'");
+        $db = mysqli_connect($server, $user, $pass, $dbName);
+        mysqli_query($db, "SET NAMES 'utf8'");
 
-    if(mysqli_connect_errno()){
-        echo 'No se puede acceder a la base de datos' . mysqli_connect_errno();
-    }else{
-        //echo 'Conexion Exitosa!!';
-    }
-
+        if(mysqli_connect_errno()){
+            echo 'No se puede acceder a la base de datos' . mysqli_connect_errno();
+        }else{
+            //echo 'Conexion Exitosa!!';
+        }
+        
+    } catch (mysqli_sql_exception $e) { 
+        throw $e; 
+    } 
 ?>
