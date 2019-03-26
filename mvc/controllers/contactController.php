6416fc6a481e33de2a7de9c30029ba8d7637aa04
unknown <pablojacobo1998@gmail.com>
2019-03-25 23:17:29 -0600

    Create void Controller Contacts

---
+++ mvc/controllers/contactController.php
@@ -0,0 +1,20 @@
+<?php
+
+class contactController {
+
+    public function addContact(){
+        //Build Logic...
+    }
+
+    public function getContacts(){
+        //Build Logic...
+    }
+
+    public function deleteContact(){
+        //Build Logic...
+    }
+
+}
+
+?>
+
