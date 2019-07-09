e8eaae756c52dfc69844c822b03d3953f80000a0
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-07-08 22:43:47 -0600

    fixes trying db

--- files/conexion.php
+++ files/conexion.php
@@ -3,6 +3,8 @@ $host = 'localhost';
 $user = 'root';
 $dbname = 'pruebasphp';
 $pass = '';
+
+//build
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
