888542fed1f85bb62a39273374157208c6a0b1a9
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-09-27 11:55:00 -0600

    dumped : mode

--- MD5-Converter/index.php
+++ MD5-Converter/index.php
@@ -9,5 +9,9 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+// get posted data
+$dataCodes = json_decode(file_get_contents("php://input"));
+var_dump( $dataCodes );
+
 
 ?>
