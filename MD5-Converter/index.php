bb8386734a332a3efa72a6b6014577683939ab84
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-09-27 12:20:06 -0600

    put a form in postman end-point

--- MD5-Converter/index.php
+++ MD5-Converter/index.php
@@ -1,15 +1,16 @@
-<?
+<?php
 
 //the references : https://www.codeofaninja.com/2017/02/create-simple-rest-api-in-php.html
+//[POST]         : http://localhost/PHPEXP/MD5-Converter/index.php
 
-// required headers
+//Headers Http
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// get posted data
+//Getting posted data in json format
 $dataCodes = json_decode(file_get_contents("php://input"));
 var_dump( $dataCodes );
 if( $dataCodes ){
