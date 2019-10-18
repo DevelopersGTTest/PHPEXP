21b4d50965a1b89cc5098106c8de97c0b91146ca
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-10-18 12:24:00 -0600

    little example collage in [mysql.x.x.]

---
+++ set-codification/add.php
@@ -0,0 +1,24 @@
+<?php
+
+if(!empty($_POST)){
+    require_once 'connection.php';
+
+    $names = $_POST['lname'];
+    $age = $_POST['age'];
+
+    $query = "INSERT INTO entity( names, age) VALUES('$names', $age);";
+    $result = sql($query);
+
+    if( $result ){
+        echo 'se inserto algo en la db';
+    }
+
+}
+
+?>
+
+<form action="add.php" method="POST" >
+    <label for=""> name <input type="text" name="lname" ></label>
+    <label for=""> age  <input type="text" name="age" id=""></label>
+    <input type="submit">
+</form>
---
+++ set-codification/connection.php
@@ -0,0 +1,32 @@
+<?php
+    
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbName = 'develop-data';
+
+    
+    try{
+        
+        $db = mysqli_connect($server, $user, $pass, $dbName);
+        
+        function sql($consulta){
+            global $db; 
+            $resultado;
+            $resultado = mysqli_query($db, utf8_decode($consulta));
+            return $resultado;
+        }
+
+        if(mysqli_connect_errno()){
+            echo 'No se puede acceder a la base de datos' . mysqli_connect_errno();
+        }else{
+            //echo 'Conexion Exitosa!!';
+            
+        }
+    
+    } catch (mysqli_sql_exception $e) { 
+        throw $e; 
+    } 
+
+    
+?>
