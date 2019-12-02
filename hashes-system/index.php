c9daafe59059c79c247870e3e50affa657af910e
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-01 23:20:37 -0600

    deleted hased

--- hashes-system/index.php
+++ hashes-system/index.php
@@ -1,33 +1,5 @@
 <?php
 
-
-//Encoded
-$user = 'pablo@h!cK__id';
-$other = '12';
-
-$mixed = $user ."." . $other;
-
-$data_encoded = base64_encode( $mixed );
-
-//var_dump( $data_encoded);
-//die();
-//Decoded
-/*
-
-fix encrypt building 13/05/2019
-
-
-$data_decoded = base64_decode( $data_encoded );
-echo $data_decoded;
-die();
-
-//individual parts
-$part_one  = explode('.', $data_decoded, 0 );
-var_dump($part_one);
-die();
-
-$part_two   explode('.', $data_decoded, 1 ); 
-*/
-
+    //say hello
 
 ?>
